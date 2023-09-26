27da66975daeb041c8d7caa0136ba82b4c082d8f
Arun Gupta <106945518+ArunGupta14@users.noreply.github.com>
2023-09-26 16:57:07 +0530

    Update game.py

--- game.py
+++ game.py
@@ -10,8 +10,6 @@ pygame.init()
 
 # create the game window
 win = pygame.display.set_mode((700,550))
-# icon = pygame.image.load() # create icon
-# pygame.display.set_icon(icon) # set icon
 pygame.display.set_caption("Star War")
 
 # background image
@@ -19,7 +17,7 @@ bg = pygame.image.load("pics//bg.jpg")
 
 # Background sound
 mixer.music.load('bgSound.mp3')
-mixer.music.play(-1) #if we give -1 once it at as a loop play continue if we not provide any value it will play once only
+mixer.music.play(-1) 
 
 # Player
 PlayerX = 0
@@ -43,23 +41,19 @@ enemyWidth = 30
 
 for i in range(numOfEnemies):
     enemyImg.append(pygame.image.load('pics//enemy.png'))
-    EnemyX.append(random.randint(0,250)) #enemy show random position
+    EnemyX.append(random.randint(0,250)) 
     EnemyY.append(random.randint(50,150))
     enemyX_change.append(4)
-    enemyY_change.append(40)#enemy movement down
+    enemyY_change.append(40)
  
 
-
 # Bullet
-# ready - You can't see the bullet on the screen
-# fire - the bullet is currently moving
 bulletImg = pygame.image.load('pics//bullet.png')
 bulletX = 0
 bulletY = 450
 bulletX_change = 0
 bulletY_change = 10 
 bullet_state = "ready"
-
 
 # Score 
 scoreNo = 0 
@@ -74,7 +68,6 @@ GameOverTextX = 250
 GameOverTextY = 200
 
 def showScore(x,y):
-    # render the text if we want render we give 4 values 2 value will be string 3 value will be boolian 4 value is rgbcolor
     score = font.render("Score : " +str(scoreNo), True, (255,255,255))
     win.blit(score,(x,y))
 
@@ -90,8 +83,8 @@ def enemy(x,y,i):
 def fire_bullet(x,y):
     global bullet_state
     bullet_state = "fire"
-    win.blit(bulletImg,(x+16,y)) # x+16,y formula get hit & try.it show bullet fire in center in the ship
-
+    win.blit(bulletImg,(x+16,y)) 
+    
 def isCollision(enemyX,enemyY,bulletX,bulletY):
     distance = math.sqrt((math.pow(enemyX-bulletX,2))+(math.pow(enemyY-bulletY,2)))
     if distance < 27:
@@ -103,7 +96,6 @@ def gameOver(x,y):
     GameOver_font = font.render("GAME OVER", True, (0,255,0))
     win.blit(GameOver_font, (x,y))
     
-
 clock = pygame.time.Clock()
 
 run = True
@@ -121,8 +113,7 @@ while run:
 
     # PlayerX Moves Left and its boudaries
     if keys[pygame.K_LEFT] and PlayerX > speed:
-        PlayerX -= speed
-        
+        PlayerX -= speed  
     
     # PlayerX Moves Right and its boudaries
     if keys[pygame.K_RIGHT] and PlayerX < 690 - width - speed:
@@ -130,19 +121,17 @@ while run:
     
     if keys[pygame.K_SPACE]:
         if bullet_state == "ready":
-        # GEt the current x cordinate of the spaceship
             bulletSound = mixer.Sound('bulletFire.wav')
             bulletSound.play()
             bulletX = PlayerX 
             fire_bullet(bulletX,bulletY)
-
 
     # Enemy Movement
     for i in range(numOfEnemies):
 
         # GameOver if enemy fall this cordinate
         if EnemyY[i] > 400:
-            for j in range(numOfEnemies):#No of y cordinate if enemy fall 400 Ycordinate than gameOver   
+            for j in range(numOfEnemies): 
                 EnemyX[j] = 2000
                 gameOver(GameOverTextX,GameOverTextY)
                 
@@ -151,14 +140,13 @@ while run:
                 run = False           
                 break
 
-
         EnemyX[i] += enemyX_change[i]
         if EnemyX[i] <= 0:
             enemyX_change[i] = 5
-            EnemyY[i] += enemyY_change [i]#enemy movent go down when he hit 0 boudaries
+            EnemyY[i] += enemyY_change [i]
         elif EnemyX[i] >= 690 - enemySpeed - width:
             enemyX_change[i] = -5
-            EnemyY[i] += enemyY_change[i] #enemy movent go down when he hit 690{last boudari} boudaries
+            EnemyY[i] += enemyY_change[i]
 
         # Collision
         collision = isCollision(EnemyX[i],EnemyY[i],bulletX,bulletY)
@@ -186,20 +174,17 @@ while run:
         fire_bullet(bulletX,bulletY)
         bulletY -= bulletY_change
 
-
-
     # Player fun call
     player(PlayerX,PlayerY)
+    
     # score fun call
     showScore(textX,textY)
-
 
     # Update the display
     pygame.display.flip()
   
     # cap the frame rate
     clock.tick(FPS)
-
 
 # quit the game
 pygame.quit()
